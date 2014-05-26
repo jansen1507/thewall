893e43438e2e8788e0e3e990ddbca4d437955c37
remythemsen <iam@stoked.dk>
2014-05-26 23:14:20 +0200

    Add app folder, and index php file

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+/* This is the entry point of the web application */
+
+// Set site path
+define('__SITE_PATH', preg_replace('/public$/', '', realpath(dirname(__FILE__))));
+
+// Run init
+require(__SITE_PATH.'app/bootstrap/init.php');
+
+// Get app object
+$app = new App();
+
+// Start application
+$app->run();
