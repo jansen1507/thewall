618579d78e8b6c1d9f244625c19e7d221a4ad1ea
remythemsen <iam@stoked.dk>
2014-05-29 14:17:37 +0200

    removed login redirection

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,12 +2,6 @@
 
 class HomeController extends Controller {
     function getIndex() {
-        if(Auth::check()) {
-            // render the view
-            $this->view->render('home/index');
-        }
-        else {
-            URL::redirect('user/login');
-        }
+        $this->view->render('home/index');
     }
 }
