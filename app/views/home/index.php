f32d3ce44f07ac36974a9e1167902d863bce9ea6
remythemsen <iam@stoked.dk>
2014-05-29 14:20:47 +0200

    removed hello message

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,4 +1,2 @@
 <div class="row">
-    <h3>Hi <?php echo ucfirst(Auth::user()->getEmail()); ?>, Welcome to <?php echo Config::get()->general->pagetitle; ?>!</h3>
-        <span class="light label headline"><h5>This is the Dashboard View</h5></span>
 </div>
