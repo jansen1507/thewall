c04432916d3a11eb4c95131de205ad15748d1948
remythemsen <iam@stoked.dk>
2014-05-31 17:39:52 +0200

    Change uses to just Helpers\

--- app/views/nav.php
+++ app/views/nav.php
@@ -1,4 +1,4 @@
-<?php use TheWall\Helpers\Auth; ?>
+<?php use TheWall\Helpers; ?>
 <nav id="navbar-main-nav" class="navbar">
     <div class="row">
         <div class="four columns" id="main-logo">
@@ -6,11 +6,17 @@
         </div>
         <nav class="eight columns navbar">
             <ul id="main-menu">
-                <li id="create-btn"><a>Create</a></li>
-                <?php if(!Auth::check()) : ?>
-                    <li id="login-btn"><a>Login</a></li>
+
+                <?php if(!Helpers\Auth::check()) : ?>
+                    <form action="http://localhost:8888/thewall/public/user/login" method="post">
+                        <li class="field"><input class="medium input" placeholder="Email" type="text" name="email" /></li>
+                        <li class="field"><input class="medium input" placeholder="Password" type="password" name="password" /></li>
+                        <li class="field"><input type="submit" class="primary medium btn" value="Login"></li>
+                    </form>
+                    <li> <span style="color:white";>OR</span> </li>
                 <?php endif; ?>
-                <?php if(Auth::check()): ?>
+                <li id="create-btn"><a>Create Account</a></li>
+                <?php if(Helpers\Auth::check()): ?>
                     <li><a href="<?php echo BASE_URL.'user/logout'; ?>" alt="logout">Logout</a></li>
                 <?php endif; ?>
             <ul>
