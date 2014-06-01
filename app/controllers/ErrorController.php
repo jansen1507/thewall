11b81e0485a9728aa1e90c83e17e26cdff835f1a
remythemsen <iam@stoked.dk>
2014-06-01 17:48:58 +0200

    Changing rendered view to not found

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -1,15 +1,14 @@
-<?php 
+<?php
+
+use TheWall\Core\Helpers\Notifier;
+
 class ErrorController extends Controller {
 
     public function getIndex() {
-        
-        // render view
-        $this->view->render('error/index');
 
-    }
-
-    public function not_found() {
-        Notifier::add('warning', 'Error: 404, Page not found');
         $this->view->render('error/not_found');
+
     }
+
+
 }
