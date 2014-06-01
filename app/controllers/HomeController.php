4f20f9896a89eb30cfa6cd825d74b450488d4b43
remythemsen <iam@stoked.dk>
2014-06-01 17:50:22 +0200

    Adding retrieval of posts to getIndex

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,7 +3,14 @@
 class HomeController extends Controller {
     function getIndex() {
 
+        // Retrieve All posts and pass to view
+        $this->view->posts = PostQuery::create()->find();
+        // Render the view.
         $this->view->render('home/index');
+
+    }
+
+    function postPost() {
 
     }
 }
