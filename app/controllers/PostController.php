9957261be3575a2bdab5b2a9e963b196e96c505b
remythemsen <iam@stoked.dk>
2014-06-01 18:26:53 +0200

    Added postPost method

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -1,9 +1,26 @@
 <?php
 
-use TheWall\Core\Helpers\URL;
+use TheWall\Core\Helpers;
 
 class PostController extends Controller {
-    function getIndex() {
-        URL::redirect('error');
+    function postPost() {
+        // get + trim vars
+        $text = (isset($_POST['text']) ? trim($_POST['text']) : false);
+
+        // Validation
+
+        if(Helpers\Validator::check(array(
+            'text' => $text
+        ))) {
+
+            $post = new Post();
+            $post->setText($text);
+            $post->setUserId(Helpers\Session::get('user_id'));
+
+            // Persist post.
+            $post->save();
+        }
+
+        Helpers\URL::redirect('home');
     }
-} 
+}
