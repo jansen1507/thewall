02a02e24ff432d6c93d8ce9322f7542cbdbc4d7f
remythemsen <iam@stoked.dk>
2014-06-01 17:54:18 +0200

    removing getIndex method

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -1,14 +1,8 @@
 <?php
 
-use TheWall\Core\Helpers\Notifier;
-
 class ErrorController extends Controller {
 
-    public function getIndex() {
 
-        $this->view->render('error/not_found');
-
-    }
 
 
 }
