212083ad824732e083792ac755f8c50f33d4de1a
remythemsen <iam@stoked.dk>
2014-06-01 20:28:43 +0200

    Adding Comments

--- app/controllers/CommentController.php
+++ app/controllers/CommentController.php
@@ -1,9 +1,28 @@
 <?php
 
+use TheWall\Core\Helpers;
 
 class CommentController extends Controller {
     function postCreate() {
-        echo 'comment posted!';
-        die();
+        // get + trim vars
+        $text = (isset($_POST['text']) ? trim($_POST['text']) : false);
+        $postId = (isset($_POST['post_id']) ? trim($_POST['post_id']) : false);
+
+        // Validation
+
+        if(Helpers\Validator::check(array(
+            'text' => $text
+        ))) {
+
+            $comment = new Comment();
+            $comment->setText($text);
+            $comment->setUserId(Helpers\Session::get('user_id'));
+            $comment->setPostId($postId);
+
+            // Persist comment.
+            $comment->save();
+        }
+
+        Helpers\URL::redirect('home');
     }
-} 
+}
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -18,18 +18,25 @@
         </div>
         <div class="row">
             <?php foreach($this->posts as $post) :?>
-                <div class="white-box row">
-                    <p><?php echo $post->getUser()->getEmail(); ?></p>
-                    <p><?php echo $post->getText(); ?></p>
-                    <p><?php echo 'comments: '; ?></p>
-                    <p>
+                <div class="row">
+                    <div class="white-box twelve columns" style="margin-bottom:20px;">
+                        <p><?php echo $post->getUser()->getEmail(); ?></p>
+                        <p><?php echo $post->getText(); ?></p>
+                        <p><?php echo 'comments: '; ?></p>
+                        <?php foreach($post->getComments() as $comment) : ?>
+                            <p><?php echo $comment->getText(); ?></p>
+                        <?php endforeach; ?>
                         <form action="<?php echo BASE_URL.'comment/create'; ?>" method="post">
                             <ul>
-                                <li class="append field"><input type="text" placeholder="Write something!" name="text" class="small input" style="max-width:52%;" /></li>
-                                <li class="append field"><button class="primary btn medium" style="font-size: 15px;font-weight:100;width:50px;color:white;background-color:#3b5998;">post</button></li>
+                                <li class="append field">
+                                    <input type="text" placeholder="Write comment!" name="text" class="small input" style="max-width: 90%" />
+                                    <input type="hidden" name="post_id" value="<?php echo $post->getId(); ?>" />
+                                    <button class="primary btn medium" style="font-size: 15px;font-weight:100;width:50px;color:white;background-color:#3b5998;">post</button>
+                                </li>
                             </ul>
                         </form>
-                    </p>
+
+                    </div>
                 </div>
             <?php endforeach;?>
         </div>
